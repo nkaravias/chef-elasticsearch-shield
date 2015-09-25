30d94f53dc3ebfdeb3bbc7ec8a5866bc88348b7a
Nikolas Karavias <nikolas.karavias@oracle.com>
2015-09-25 13:48:39 -0400

    Remove obsolete library method

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -11,17 +11,5 @@ module Elasticsearch
         return nil
       end
     end
-
-    def render_enclosed(hash,key,separator=':',prefix='',suffix='')
-      value = hash[key]
-      unless value.nil?
-        if value.is_a?(Array)
-          return [prefix,key,separator,value.to_s,suffix,"\n"].join
-        else
-          return [prefix,key,separator,value.to_s,suffix,"\n"].join
-        end
-        return nil
-      end
-    end
   end
 end
