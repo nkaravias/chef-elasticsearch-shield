ad156c826ced0d1ecdb2b12419a94c9df8d05fcf
Nikolas Karavias <nikolas.karavias@oracle.com>
2016-06-09 11:17:22 -0400

    [EDO-6813] Allow users to access mappings

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'erd_devops_us_grp@oracle.com'
 license 'All rights reserved'
 description 'Installs/Configures omc_elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.1'
+version '0.4.2'
 
 depends 'yum'
 depends 'java'
