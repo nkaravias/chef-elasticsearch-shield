08e5ac11525ec5815d0d2de60836b82eea82475d
Nikolas Karavias <nikolas.karavias@oracle.com>
2016-07-26 15:31:46 -0400

    Final changes for roles.yml
    make public

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 name 'omc_elasticsearch'
-maintainer 'Oracle Marketing Cloud DevOps team'
-maintainer_email 'erd_devops_us_grp@oracle.com'
+maintainer 'Nikolas Karavias'
+maintainer_email 'nikolas.karavias@oracle.com'
 license 'All rights reserved'
 description 'Installs/Configures omc_elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
